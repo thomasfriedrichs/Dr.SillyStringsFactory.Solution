ae14e10c8bf29d547ba2f5c294233c00c39cdac8
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-28 10:55:54 -0700

    fix startup

--- StringFactory/Program.cs
+++ StringFactory/Program.cs
@@ -1,7 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList
+namespace StringFactory
 {
   public class Program
   {
--- StringFactory/Startup.cs
+++ StringFactory/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ToDoList.Models;
+using StringFactory.Models;
 
-namespace ToDoList
+namespace StringFactory
 {
   public class Startup
   {
@@ -25,7 +25,7 @@ namespace ToDoList
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<ToDoListContext>(options => options
+        .AddDbContext<StringFactoryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
